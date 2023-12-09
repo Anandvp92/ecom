e3a25b9161d7a55fbc4073be14608f6f7ead8686
Anandvp <anandvp92@gmail.com>
2023-12-09 15:31:28 +0530

    thirdupdate

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
 def userlogin(request):
+    if request.POST:
+        print(request.POST['username'])
+        print(request.POST['password'])
+        return redirect("index")
+    return render(request,"login.html")
+
+def register(request):
+    if request.POST:
+        print(request.POST['username'])        
+        print(request.POST['email'])
+        print(request.POST['password'])
+        return redirect("index")
     return render(request,"login.html")
